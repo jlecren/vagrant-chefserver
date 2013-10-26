f4a179aa2e8f8756b789ad779d7c67dd2cadc46a
Joran Le Cren <joran.lecren@prologism.fr>
2013-10-26 02:20:53 +0200

    Use the Vagrantfile of the chef-server cookbook

--- vagrant/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-# download curl
-sudo apt-get -y install curl
-# install chef-solo
-curl -L https://www.opscode.com/chef/install.sh | sudo bash
-# create required bootstrap dirs/files
-sudo mkdir -p /var/chef/cache
